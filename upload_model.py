f53fad5fbdea88c67b0ba302720194047d0e462d
Arjuna3667 <mallikarjuna.maddu@gmail.com>
2026-03-24 18:23:02 +0530

    Update upload_model.py

--- upload_model.py
+++ upload_model.py
@@ -1,15 +1,18 @@
-from huggingface_hub import HfApi
 import os
-
-api = HfApi()
+from huggingface_hub import HfApi
 
 print("FILES BEFORE UPLOAD:", os.listdir())
+
+if not os.path.exists("model.pkl"):
+    raise FileNotFoundError(" model.pkl NOT FOUND before upload")
+
+api = HfApi()
 
 api.upload_file(
     path_or_fileobj="model.pkl",
     path_in_repo="model.pkl",
-    repo_id="Arjuna3667/tourism-model",   
-    repo_type="model",                    
+    repo_id="Arjuna3667/tourism-model",
+    repo_type="model",
     token=os.environ["HF_TOKEN"]
 )
 
