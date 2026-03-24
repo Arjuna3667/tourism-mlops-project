064df084201fca6937273ed14434fd24dfb62617
Arjuna3667 <mallikarjuna.maddu@gmail.com>
2026-03-24 12:00:04 +0530

    Update train.py

--- train.py
+++ train.py
@@ -1,32 +1,34 @@
 import pandas as pd
-import joblib
-from xgboost import XGBClassifier
+from sklearn.ensemble import RandomForestClassifier
 from imblearn.over_sampling import SMOTE
+import joblib
 
-# Load dataset from Hugging Face
-df = pd.read_csv(
-    "https://huggingface.co/datasets/Arjuna3667/tourism-package-data/resolve/main/tourism.csv"
-)
+# Load data
+df = pd.read_csv("data/train.csv")
 
+# KEEP ONLY REQUIRED COLUMNS
+df = df[[
+    "Age",
+    "MonthlyIncome",
+    "NumberOfTrips",
+    "Passport",
+    "CityTier",
+    "ProdTaken"
+]]
+
+# Split
 X = df.drop("ProdTaken", axis=1)
 y = df["ProdTaken"]
 
-# Balance data
+# Handle imbalance
 sm = SMOTE(random_state=42)
 X_res, y_res = sm.fit_resample(X, y)
 
 # Train model
-model = XGBClassifier(
-    n_estimators=200,
-    max_depth=5,
-    learning_rate=0.1,
-    random_state=42
-)
-
+model = RandomForestClassifier(random_state=42)
 model.fit(X_res, y_res)
-
-# Save feature names
-model.feature_names = X.columns.tolist()
 
 # Save model
 joblib.dump(model, "model.pkl")
+
+print("Model trained successfully!")
