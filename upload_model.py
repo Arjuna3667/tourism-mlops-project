c920631875804d98c81440717ee5e3ad33b03e59
Arjuna3667 <mallikarjuna.maddu@gmail.com>
2026-03-24 18:04:50 +0530

    Update upload_model.py

--- upload_model.py
+++ upload_model.py
@@ -1,15 +1,15 @@
 from huggingface_hub import HfApi
 import os
 
-print("FILES BEFORE UPLOAD:", os.listdir())
-
 api = HfApi()
+
+print("FILES BEFORE UPLOAD:", os.listdir())
 
 api.upload_file(
     path_or_fileobj="model.pkl",
     path_in_repo="model.pkl",
-    repo_id="Arjuna3667/tourism-package-data",
-    repo_type="dataset",
+    repo_id="Arjuna3667/tourism-model",   
+    repo_type="model",                    
     token=os.environ["HF_TOKEN"]
 )
 
